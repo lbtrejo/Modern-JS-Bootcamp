165cb96ec5949a4ad0cf0b4aef1869a74f13ba86
Levi Trejo <levi.b.trejo@gmail.com>
2021-01-23 19:52:40 -0600

    feat: html file for script code, app file for script writing and review

---
+++ Program-Logic-Flow/app.js
@@ -0,0 +1,21 @@
+// alert("This is an alert!");
+
+if (1 === 1){
+    console.log("it's true!")
+}
+
+// example 2
+
+let rating = 3;
+
+if (rating === 3) {
+    console.log("You are a superstar!");
+}
+
+
+// Example 3
+let num = 38;
+
+if (num % 2 !== 0){
+    console.log("Odd number")
+}
