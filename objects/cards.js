1b4e42fe5d703c0d84e2fcf330a2331084f257c4
Levi Trejo <levi.b.trejo@gmail.com>
2021-01-27 14:25:24 -0600

    feat: object method section complete

--- objects/cards.js
+++ objects/cards.js
@@ -29,48 +29,103 @@
 
 // Build it using OOP principals :D
 
-const myDeck = {
-    deck: [],  // deck to draw from
-    drawnCards: [], // discard pile
-    suits: ['clubs', 'spades', 'diamonds', 'hearts'],
-    values: '2,3,4,5,6,7,8,9,10,J,Q,K,A',
-    initializeDeck(){
-        const { suits, values, deck } = this;
-        for (let value of values.split(',')) {
-            for (let suit of suits) {
-                deck.push({
-                    value,
-                    suit
-                })
+// const myDeck = {
+//     deck: [],  // deck to draw from
+//     drawnCards: [], // discard pile
+//     suits: ['clubs', 'spades', 'diamonds', 'hearts'],
+//     values: '2,3,4,5,6,7,8,9,10,J,Q,K,A',
+//     initializeDeck(){
+//         const { suits, values, deck } = this;
+//         for (let value of values.split(',')) {
+//             for (let suit of suits) {
+//                 deck.push({
+//                     value,
+//                     suit
+//                 })
+//             }
+//         }
+//     },
+//     drawCard() {
+//         const card = this.deck.pop();
+//         this.drawnCards.push(card);
+//         return card;
+//     },
+//     drawMultiple(numCards) {
+//         const hand = [];
+//         for (let i = 0; i < numCards; i++) {
+//             hand.push(this.drawCard());
+//         }
+//         return hand;
+//     },
+//     shuffle() {  // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+//         const { deck } = this;
+//         // loop over array backwards
+//         for (let i = deck.length - 1; i > 0; i--) {
+//             // pick random index before current element
+//             let j = Math.floor(Math.random() * (i + 1));
+//             // swap deck[i] and deck[j]
+//             [deck[i], deck[j]] = [deck[j], deck[i]];
+//         }
+//     }
+// }
+
+// myDeck.initializeDeck();
+// myDeck.shuffle();
+// let h1 = myDeck.drawMultiple(2);
+// let h2 = myDeck.drawMultiple(2);
+// let p1 = myDeck.drawMultiple(5);
+
+
+// Making a deck factory
+
+const makeDeck = () => {
+    return {
+        deck: [],  // deck to draw from
+        drawnCards: [], // discard pile
+        suits: ['clubs', 'spades', 'diamonds', 'hearts'],
+        values: '2,3,4,5,6,7,8,9,10,J,Q,K,A',
+        initializeDeck(){
+            const { suits, values, deck } = this;
+            for (let value of values.split(',')) {
+                for (let suit of suits) {
+                    deck.push({
+                        value,
+                        suit
+                    })
+                }
             }
-        }
-    },
-    drawCard() {
-        const card = this.deck.pop();
-        this.drawnCards.push(card);
-        return card;
-    },
-    drawMultiple(numCards) {
-        const hand = [];
-        for (let i = 0; i < numCards; i++) {
-            hand.push(this.drawCard());
-        }
-        return hand;
-    },
-    shuffle() {  // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
-        const { deck } = this;
-        // loop over array backwards
-        for (let i = deck.length - 1; i > 0; i--) {
-            // pick random index before current element
-            let j = Math.floor(Math.random() * (i + 1));
-            // swap deck[i] and deck[j]
-            [deck[i], deck[j]] = [deck[j], deck[i]];
+        },
+        drawCard() {
+            const card = this.deck.pop();
+            this.drawnCards.push(card);
+            return card;
+        },
+        drawMultiple(numCards) {
+            const hand = [];
+            for (let i = 0; i < numCards; i++) {
+                hand.push(this.drawCard());
+            }
+            return hand;
+        },
+        shuffle() {  // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+            const { deck } = this;
+            // loop over array backwards
+            for (let i = deck.length - 1; i > 0; i--) {
+                // pick random index before current element
+                let j = Math.floor(Math.random() * (i + 1));
+                // swap deck[i] and deck[j]
+                [deck[i], deck[j]] = [deck[j], deck[i]];
+            }
         }
     }
 }
 
+const myDeck = makeDeck();
 myDeck.initializeDeck();
 myDeck.shuffle();
 let h1 = myDeck.drawMultiple(2);
 let h2 = myDeck.drawMultiple(2);
 let p1 = myDeck.drawMultiple(5);
+
+const deck2 = makeDeck();
+deck2.initializeDeck();
