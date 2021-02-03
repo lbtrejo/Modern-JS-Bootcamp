ab33527ecdbeb5ca7d9b876c280ad0e769aefaf9
Levi Trejo <levi.b.trejo@gmail.com>
2021-02-03 13:51:51 -0600

    feat: events on mult objects

--- dom-events/dom-events.js
+++ dom-events/dom-events.js
@@ -1,4 +1,4 @@
-const btn = document.querySelector('button');
+// const btn = document.querySelector('button');
 
 // btn.onclick = function () {
 //     console.log('Click me!');
@@ -32,17 +32,42 @@ const btn = document.querySelector('button');
 // })
 
 // lulz
-btn.addEventListener('mouseover', function () {
-    const randHgt = Math.floor(Math.random() * window.innerHeight);
-    const randWdt = Math.floor(Math.random() * window.innerWidth);
-    btn.style.top = `${randHgt}px`;
-    btn.style.left = `${randWdt}px`;
-})
+// btn.addEventListener('mouseover', function () {
+//     const randHgt = Math.floor(Math.random() * window.innerHeight);
+//     const randWdt = Math.floor(Math.random() * window.innerWidth);
+//     btn.style.top = `${randHgt}px`;
+//     btn.style.left = `${randWdt}px`;
+// })
 
-// window.screen returns an object showing the full dimensions of the screen, not useful for this example but good to know.
+// // window.screen returns an object showing the full dimensions of the screen, not useful for this example but good to know.
 
-// if the user manages to click
-btn.addEventListener('click', function () {
-    btn.innerText = 'You got me!';
-    document.body.style.backgroundColor = 'green';
+// // if the user manages to click
+// btn.addEventListener('click', function () {
+//     btn.innerText = 'You got me!';
+//     document.body.style.backgroundColor = 'green';
+// })
+
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+const container = document.querySelector('#boxes');
+
+const h1 = document.querySelector('h1');
+
+const printColor = function () {
+    
+    // console.log(this);
+    // console.log(this.style.backgroundColor);
+    h1.style.color = this.style.backgroundColor;
+}
+
+for (let color of colors) {
+    const colorDiv = document.createElement('div');
+    colorDiv.style.backgroundColor = color;
+    colorDiv.classList.add('box');
+    container.append(colorDiv);
+    colorDiv.addEventListener('click', printColor);
+}
+
+h1.addEventListener('mouseover', function () {
+    console.log(this.innerText);
 })
