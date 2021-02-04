a094110384923c99e569c10fd5146a39f07188a0
Levi Trejo <levi.b.trejo@gmail.com>
2021-02-04 12:14:31 -0600

    feat: refactor of callback hell w promises complete

--- async-callbacks-promises/callback-hell/callback-hell.js
+++ async-callbacks-promises/callback-hell/callback-hell.js
@@ -16,21 +16,21 @@ const btn = document.querySelector('button');
 //     }, 1000);
 // }, 1000)
 
-const moveX = (element, amount, delay, onSuccess, onFailure) => {
-        setTimeout(() => {
-            const bodyBoundary = document.body.clientWidth;
-            const currRight = element.getBoundingClientRect().right;
-            const currLeft = element.getBoundingClientRect().left;
+// const moveX = (element, amount, delay, onSuccess, onFailure) => {
+//         setTimeout(() => {
+//             const bodyBoundary = document.body.clientWidth;
+//             const currRight = element.getBoundingClientRect().right;
+//             const currLeft = element.getBoundingClientRect().left;
 
-            if (currRight + amount > bodyBoundary) {
-                onFailure();
-            }
-            else {
-                element.style.transform = `translateX(${amount + currLeft}px)`
-                onSuccess();
-            }
-    }, delay)
-};
+//             if (currRight + amount > bodyBoundary) {
+//                 onFailure();
+//             }
+//             else {
+//                 element.style.transform = `translateX(${amount + currLeft}px)`
+//                 onSuccess();
+//             }
+//     }, delay)
+// };
 
 // moveX(btn, 100, 1000, function(){
 //     moveX(btn, 100, 1000, function(){
@@ -42,25 +42,82 @@ const moveX = (element, amount, delay, onSuccess, onFailure) => {
 //     });
 // })
 
-moveX(btn, 100, 1000, () => {
-    // success
-    moveX(btn, 400, 1000, () => {
-        // success
-        moveX(btn, 1000, 1000, () => {
-            // success
-            console.log('Really?');
-        }, () => {
-            // failure
-            alert('Cannot move further');
-        })
-    }, () => {
-        // failure
-        alert('Cannot move further');
-    })
-}, () => {
-    // failure
-    alert('Cannot move further');
-})
+// moveX(btn, 100, 1000, () => {
+//     // success
+//     moveX(btn, 400, 1000, () => {
+//         // success
+//         moveX(btn, 1000, 1000, () => {
+//             // success
+//             console.log('Really?');
+//         }, () => {
+//             // failure
+//             alert('Cannot move further');
+//         })
+//     }, () => {
+//         // failure
+//         alert('Cannot move further');
+//     })
+// }, () => {
+//     // failure
+//     alert('Cannot move further');
+// })
 
 // A promise is an object representing the eventual completion or failure of an asyunchronous operation
 
+// Refactoring above to utilize promise chaining
+
+const moveX = (element, amount, delay) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const bodyBoundary = document.body.clientWidth;
+            const currRight = element.getBoundingClientRect().right;
+            const currLeft = element.getBoundingClientRect().left;
+    
+            if (currRight + amount > bodyBoundary) {
+                reject({ bodyBoundary, currRight, amount});
+            }
+            else {
+                element.style.transform = `translateX(${amount + currLeft}px)`;
+                resolve();
+            }
+        }, delay);
+    })
+};
+
+// moveX(btn, 300, 1000)
+//     .then(() => {
+//         console.log('button moved');
+//         return moveX(btn, 300, 1000);
+//     })
+//     .then(() => {
+//         console.log('button moved again')
+//         return moveX(btn, 300, 1000);
+//     })
+//     .then(() => {
+//         console.log('button moved again')
+//         return moveX(btn, 300, 1000);
+//     })
+//     .then(() => {
+//         console.log('button moved again')
+//         return moveX(btn, 300, 1000);
+//     })
+//     .then(() => {
+//         console.log('button moved again')
+//         return moveX(btn, 300, 1000);
+//     })
+//     .catch((err) => {
+//         console.error('Out of space, cannot move');
+//     })
+
+// Making more efficient with IMPLICIT return
+    moveX(btn, 300, 1000)
+    .then(() => moveX(btn, 300, 1000))
+    .then(() => moveX(btn, 300, 1000))
+    .then(() => moveX(btn, 300, 1000))
+    .then(() => moveX(btn, 300, 1000))
+    .then(() => moveX(btn, 300, 1000))
+    .catch(({ bodyBoundary, currRight, amount }) => {
+        console.error('Out of space, cannot move');
+        console.log(`Body is ${bodyBoundary}px wide.`);
+        console.log(`Element is at ${currRight}, ${amount}px is too large!`)
+    })
