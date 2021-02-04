50396829ca42300a86d2c486e443b744f183ea8e
Levi Trejo <levi.b.trejo@gmail.com>
2021-02-03 20:46:01 -0600

    feat: callback hell is a thing

---
+++ async-callbacks-promises/callback-hell.js
@@ -0,0 +1,63 @@
+const btn = document.querySelector('button');
+
+// setTimeout(() => {
+//     btn.style.transform = `translateX(100px)`;
+//     setTimeout(() => {
+//         btn.style.transform = `translateX(200px)`;
+//         setTimeout(() => {
+//             btn.style.transform = `translateX(300px)`;
+//             setTimeout(() => {
+//                 btn.style.transform = `translateX(400px)`;
+//                 setTimeout(() => {
+//                     btn.style.transform = `translateX(500px)`;
+//                 }, 1000);
+//             }, 1000);
+//         }, 1000);
+//     }, 1000);
+// }, 1000)
+
+const moveX = (element, amount, delay, onSuccess, onFailure) => {
+        setTimeout(() => {
+            const bodyBoundary = document.body.clientWidth;
+            const currRight = element.getBoundingClientRect().right;
+            const currLeft = element.getBoundingClientRect().left;
+
+            if (currRight + amount > bodyBoundary) {
+                onFailure();
+            }
+            else {
+                element.style.transform = `translateX(${amount + currLeft}px)`
+                onSuccess();
+            }
+    }, delay)
+};
+
+// moveX(btn, 100, 1000, function(){
+//     moveX(btn, 100, 1000, function(){
+//         moveX(btn, 100, 1000, function(){
+//             moveX(btn, 100, 1000, function(){
+//                 moveX(btn, 1000, 1000);
+//             });
+//         });
+//     });
+// })
+
+moveX(btn, 100, 1000, () => {
+    // success
+    moveX(btn, 400, 1000, () => {
+        // success
+        moveX(btn, 1000, 1000, () => {
+            // success
+            console.log('Really?');
+        }, () => {
+            // failure
+            alert('Cannot move further');
+        })
+    }, () => {
+        // failure
+        alert('Cannot move further');
+    })
+}, () => {
+    // failure
+    alert('Cannot move further');
+})
