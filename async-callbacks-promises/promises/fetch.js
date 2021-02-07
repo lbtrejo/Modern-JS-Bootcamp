0cd68b609c9b6eebc5257865730787d7b8454679
Levi Trejo <levi.b.trejo@gmail.com>
2021-02-06 21:04:23 -0600

    feat: fetch examples

--- async-callbacks-promises/promises/fetch.js
+++ async-callbacks-promises/promises/fetch.js
@@ -20,31 +20,41 @@ prom.then((response) => {
     console.error(err);
 })
 
+const checkStatusAndParse = (response) => {
+    console.log(response);
+    if (!response.ok) {
+        throw new Error(`Status Code Error: ${response.status}`);
+    }
+    else {
+        return response.json();
+    }
+}
+
 fetch('https://swapi.dev/api/planets/')
     .then((response) => {
-        if (!response.ok) {
-            throw new Error(`Status Code Error: ${response.status}`);
-        }
-        else {
-            return response.json();
-        }
+        return checkStatusAndParse(response);
     })
     .then((data) => {
         console.log('Fetched all planets (first 10)');
+        // console.log(data);
+        for (let planet of data.results) {
+            console.log(planet.name);
+        }
+        const nextURL = data.next;
         const filmURL = data.results[0].films[0];
-        return fetch(filmURL);
+        return fetch(nextURL);
+        // return fetch(filmURL);
     })
     .then((response) => {
-        if (!response.ok) {
-            throw new Error(`Status Code Error: ${response.status}`);
-        }
-        else {
-            return response.json();
-        }
+        return checkStatusAndParse(response);
     })
     .then((data) => {
-        console.log('Fetched first firm, based off first planet');
-        console.log(data.title);
+        // console.log('Fetched first firm, based off first planet');
+        console.log('Fetched next 10 planets')
+        // console.log(data.title);
+        for (let planet of data.results) {
+            console.log(planet.name);
+        }
     })
     .catch((error) => {
         console.error('Fetch error: ', error);
