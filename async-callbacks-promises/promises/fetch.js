9f7c78a8a5179eb689014ef27c712144e4779b4d
Levi Trejo <levi.b.trejo@gmail.com>
2021-02-08 12:25:52 -0600

    feat: fefactored fetch chains

--- async-callbacks-promises/promises/fetch.js
+++ async-callbacks-promises/promises/fetch.js
@@ -2,43 +2,6 @@
 
 const prom = fetch('https://swapi.dev/api/planets');
 
-prom.then((response) => {
-    //console.log(response); // response object, has interesting properties such as status, ok, and a body property
-    //console.log(response.json()); // The json method reads the stream to completion, but takes time so it returns a promise as well
-    if (!response.ok) {
-        throw new Error(`Status Code Error: ${response.status}`);
-        // response.json().then((data) => {
-        //     console.log(data);
-        //     for (let planet of data.results) {
-        //         console.log(planet.name);
-        //     }
-        // })
-    }
-    return response.json();
-})
-.then((data) => {
-    // console.log(data);
-    // for (let planet of data.results) {
-        // console.log(planet);
-        // console.log(planet.name);
-    // }
-    const filmURL = data.results[0].films[0];
-    return fetch(filmURL);
-})
-.then((response) => {
-    if (!response.ok) {
-        throw new Error(`Status Code Error: ${response.status}`);
-    }
-    return response.json();
-})
-.then((data) => {
-    console.log('Fetched first film based off first planet');
-    console.log(data.title);
-})
-.catch((err) => {  // Generally requires a network failure, won't fire on 4xx or 5xx HTTP status!!!
-    console.error(err);
-})
-
 const checkStatusAndParse = (response) => {
     console.log(response);
     if (!response.ok) {
@@ -48,6 +11,35 @@ const checkStatusAndParse = (response) => {
         return response.json();
     }
 }
+
+const printPlanets = (data) => {
+    console.log('Loaded 10 planets...');
+    for (let planet of data.results) {
+        console.log(planet.name);
+    }
+    return Promise.resolve(data.next);  // Easily return a resolved promise so the next then callback can function properly
+}
+
+const fetchNextPlanets = (url = 'https://swapi.dev/api/planets/') => {
+    return fetch(url);
+};
+
+// Proper refactor after moving redundant code into functions returning promises
+
+// prom.then(checkStatusAndParse)
+fetchNextPlanets()
+    .then(checkStatusAndParse)
+    .then(printPlanets)
+    .then(fetchNextPlanets)
+    .then(checkStatusAndParse)
+    .then(printPlanets)
+    .then(fetchNextPlanets)
+    .then(checkStatusAndParse)
+    .then(printPlanets)
+.catch((err) => {
+    console.log('Something went wrong with fetch');
+    console.log(err);
+})
 
 // fetch('https://swapi.dev/api/planets/')
 //     .then((response) => {
