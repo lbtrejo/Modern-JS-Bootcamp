4045b824724a755219eacb91e6697945731fb58b
Levi Trejo <levi.b.trejo@gmail.com>
2021-02-08 14:15:31 -0600

    feat: multiple awaits (chaining)

--- async-await/async-await.js
+++ async-await/async-await.js
@@ -55,4 +55,46 @@ async function getPlanets() {
     }
 }
 
-getPlanets();
+// getPlanets();
+
+// updated moveX function from previous promises
+
+const moveX = (element, amount, delay) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const bodyBoundary = document.body.clientWidth;
+            const elRight = element.getBoundingClientRect().right;
+            const currLeft = element.getBoundingClientRect().left;
+
+            if(elRight + amount > bodyBoundary) {
+                reject({ bodyBoundary, elRight, amount });
+            }
+            else {
+                element.style.transform = `translateX(${currLeft + amount}px)`;
+                resolve();
+            }
+        }, delay);
+    });
+}
+
+const btn = document.querySelector('button');
+
+async function animateRight(el, amt) {
+    await moveX(el, amt, 1000)
+    await moveX(el, amt, 1000)
+    await moveX(el, amt, 1000)
+    await moveX(el, amt, 1000)
+    await moveX(el, amt, 1000)
+    await moveX(el, amt, 1000)
+    await moveX(el, amt, 1000)
+    await moveX(el, amt, 1000)
+    await moveX(el, amt, 1000)
+    await moveX(el, amt, 1000)
+    await moveX(el, amt, 1000)
+    await moveX(el, amt, 1000)
+}
+
+animateRight(btn, 100).catch((err) => {
+    console.log('Error!!!', err);
+    animateRight(btn, -100)
+});
