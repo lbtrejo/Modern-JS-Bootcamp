7014d8bb3f3bf3bd6d4feade7986f22e1aff7a88
Levi Trejo <levi.b.trejo@gmail.com>
2021-02-08 15:25:40 -0600

    feat: parallel vs sequential requests

---
+++ async-await/pokemon.js
@@ -0,0 +1,29 @@
+// Sequential requests
+
+// async function get3Pokemon() {
+//     const poke1 = await axios.get('https://pokeapi.co/api/v2/pokemon/1');
+//     const poke2 = await axios.get('https://pokeapi.co/api/v2/pokemon/2');
+//     const poke3 = await axios.get('https://pokeapi.co/api/v2/pokemon/3');
+//     console.log(poke1.data);
+//     console.log(poke2.data);
+//     console.log(poke3.data);
+// }
+
+// get3Pokemon();
+
+
+async function get3Pokemon() {
+    // Make all 3 requests at the same time
+    const prom1 = axios.get('https://pokeapi.co/api/v2/pokemon/1'); // returns a PROMISE
+    const prom2 = axios.get('https://pokeapi.co/api/v2/pokemon/2');
+    const prom3 = axios.get('https://pokeapi.co/api/v2/pokemon/3');
+    console.log(prom1);
+    const poke1 = await prom1;  // Assign the resolved promise data to your var for reference
+    const poke2 = await prom2;
+    const poke3 = await prom3;
+    console.log(poke1.data);
+    console.log(poke2.data);
+    console.log(poke3.data);
+}
+
+get3Pokemon();
