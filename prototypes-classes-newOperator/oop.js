994d71fb3e7d7cb1a31dce79854a3322fd5c8264
Levi Trejo <levi.b.trejo@gmail.com>
2021-02-09 14:51:44 -0600

    feat: JavaScript Classes

--- prototypes-classes-newOperator/oop.js
+++ prototypes-classes-newOperator/oop.js
@@ -86,6 +86,7 @@ class Color {
         this.g = g;
         this.b = b;
         this.name = name;
+        this.calcHSL();  // ensure we automatically create HSL values on color construction
     }
     greet() {
         console.log(`Hello from ${this.name}`);
@@ -104,8 +105,72 @@ class Color {
     rgba(a = 1.0) {
         return `rgba(${this.innerRGB()}, ${a})`;
     }
+    hsl() {
+        const { h, s, l } = this;
+        return `hsl(${h}, ${s}%, ${l}%)`
+    }
+    opposite () {
+        const { h, s, l } = this;
+        const newHue = (h + 180) % 360;
+        return `hsl(${newHue}, ${s}%, ${l}%)`
+    }
+    fullySaturated () {
+        const { h, s, l } = this;
+        const newSat = 100;
+        return `hsl(${h}, ${newSat}%, ${l}%)`
+    }
+    calcHSL() {
+        let { r, g, b} = this;
+        // Make r, g, and b fractions of 1
+        r /= 255;
+        g /= 255;
+        b /= 255;
+  // Find greatest and smallest channel values
+    let cmin = Math.min(r,g,b),
+        cmax = Math.max(r,g,b),
+        delta = cmax - cmin,
+        h = 0,
+        s = 0,
+        l = 0;
+    // Calculate hue
+    // No difference
+    if (delta == 0)
+        h = 0;
+    // Red is max
+    else if (cmax == r)
+        h = ((g - b) / delta) % 6;
+    // Green is max
+    else if (cmax == g)
+        h = (b - r) / delta + 2;
+    // Blue is max
+    else
+        h = (r - g) / delta + 4;
+
+    h = Math.round(h * 60);
+
+    // Make negative hues positive behind 360°
+    if (h < 0)
+        h += 360;
+
+    // Calculate lightness
+    l = (cmax + cmin) / 2;
+
+    // Calculate saturation
+    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+    // Multiply l and s by 100
+    s = +(s * 100).toFixed(1);
+    l = +(l * 100).toFixed(1);
+
+    this.h = h;
+    this.s = s;
+    this.l = l;
+    }
 }
 
 // The class keyword and constructor function are REQUIRED!!!!!
 
 const c1 = new Color(255, 67, 89, 'tomato');
+const white = new Color(255, 255, 255, 'white');
+
+
