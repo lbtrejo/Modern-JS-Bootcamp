cd4479b745bdbde846765fd6187a131fce2b3aec
Levi Trejo <levi.b.trejo@gmail.com>
2021-02-09 20:37:29 -0600

    feat: refactored timer class to its own file

---
+++ drawing-animations/timer.js
@@ -0,0 +1,54 @@
+class Timer {
+    constructor(durationInput, startButton, pauseButton, callbacks) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+        if (callbacks) {
+            this.onStart = callbacks.onStart;
+            this.onTick = callbacks.onTick;
+            this.onComplete = callbacks.onComplete;
+        }
+
+        this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
+    }
+
+    start = () => {
+        if (this.onStart) {
+            this.onStart();
+        }
+        this.tick();
+        this.intervalId = setInterval(this.tick, 1000);
+    }
+
+    pause = () => {
+        clearInterval(this.intervalId);
+    }
+
+    tick = () => {
+        if (this.timeRemaining <= 0) {
+            if (this.onComplete) {
+                this.onComplete();
+            }
+            this.pause();
+        }
+        else {
+            if (this.onTick) {
+                this.onTick();
+            }
+            this.timeRemaining = this.timeRemaining - 1;
+        }
+    }
+
+    get timeRemaining() {
+        return parseFloat(this.durationInput.value);
+    }
+
+    set timeRemaining(time) {
+        this.durationInput.value = time;
+    }
+
+    onDurationChange () {
+
+    }
+}
