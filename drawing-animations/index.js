12d71aa3f0ada4e3ae36c8c1d698c81228f0f310
Levi Trejo <levi.b.trejo@gmail.com>
2021-02-09 20:25:27 -0600

    feat: timer class functionally complete

--- drawing-animations/index.js
+++ drawing-animations/index.js
@@ -7,16 +7,33 @@ class Timer {
         this.startButton.addEventListener('click', this.start);
         this.pauseButton.addEventListener('click', this.pause);
     }
+
     start = () => {
         this.tick();
         this.intervalId = setInterval(this.tick, 1000);
     }
+
     pause = () => {
         clearInterval(this.intervalId);
     }
+
     tick = () => {
-        console.log('Timer tick...');
+        if (this.timeRemaining <= 0) {
+            this.pause();
+        }
+        else {
+            this.timeRemaining = this.timeRemaining - 1;
+        }
     }
+
+    get timeRemaining() {
+        return parseFloat(this.durationInput.value);
+    }
+
+    set timeRemaining(time) {
+        this.durationInput.value = time;
+    }
+
     onDurationChange () {
 
     }
