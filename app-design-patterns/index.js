a020cd524f9af8efa1bff6f97583bddaf58cb91f
Levi Trejo <levi.b.trejo@gmail.com>
2021-02-10 14:29:30 -0600

    feat: search widget input wired up

--- app-design-patterns/index.js
+++ app-design-patterns/index.js
@@ -1,1 +1,21 @@
-console.log('Script loaded!')
+const baseURL = 'http://www.omdbapi.com/';
+
+const fetchData =  async (searchTerm) => {
+    const response = await axios.get(baseURL, {
+        params: {
+            apikey: omdbApiKey,
+            s: searchTerm
+            // i: 'tt0848228'
+        }
+    })
+
+    console.log(response.data);
+};
+
+// fetchData();
+
+const input = document.querySelector('input');
+
+input.addEventListener('input', (event) => {
+    fetchData(event.target.value);
+})
