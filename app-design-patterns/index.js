859f76039b59b5d0b3fbf849d1ccd0eb03a63999
Levi Trejo <levi.b.trejo@gmail.com>
2021-02-11 13:05:31 -0600

    feat: auto close dropdown and handle empty response

--- app-design-patterns/index.js
+++ app-design-patterns/index.js
@@ -30,15 +30,29 @@ const dropdown = document.querySelector('.dropdown');
 const resultsWrapper = document.querySelector('.results');
 
 const onInput = async (event) => {
+    // Grab our movies result and mark await 
     const movies = await fetchData(event.target.value);
-    
+
+    // If no results are returned, remove the dropdown and return out of the function
+    if (!movies.length) {
+        dropdown.classList.remove('is-active');
+        return;
+    }
+
+    // clear the resultsWrapper if any previous content is listed
+    resultsWrapper.innerHTML = '';
+
+    // active the dropdown
     dropdown.classList.add('is-active');
+
+    // build HTML for our movie results and populate them into the wrapper
     for (let movie of movies) {
         const option = document.createElement('a');
+        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster
         option.classList.add('dropdown-item');
         
         option.innerHTML = `
-        <img src="${movie.Poster}" />
+        <img src="${imgSrc}" />
         ${movie.Title} (${movie.Year})
         `;
         resultsWrapper.appendChild(option);
@@ -46,3 +60,12 @@ const onInput = async (event) => {
 };
 
 input.addEventListener('input', debounce(onInput, 500));
+
+// Add a full document event listener for click events
+// Check to see if the target is contained in our 'root' section
+// If so, do nothing.  If not, close the dropdown.
+document.addEventListener('click', event => {
+    if (!root.contains(event.target)) {
+        dropdown.classList.remove('is-active');
+    }
+})
