edff247e4f7ae0ebcad3959481d9a0b50fb186d6
Levi Trejo <levi.b.trejo@gmail.com>
2021-02-11 12:26:52 -0600

    feat: moving html generation and repairing references

--- app-design-patterns/index.js
+++ app-design-patterns/index.js
@@ -14,17 +14,34 @@ const fetchData =  async (searchTerm) => {
     return response.data.Search
 };
 
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+    <label><b>Search for a Movie</b></label>
+    <input class="input" />
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+`;
+
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
+
 const onInput = async (event) => {
     const movies = await fetchData(event.target.value);
-
+    
+    dropdown.classList.add('is-active');
     for (let movie of movies) {
-        const movieDiv = document.createElement('div');
-        movieDiv.innerHTML = `
+        const option = document.createElement('a');
+        option.classList.add('dropdown-item');
+        
+        option.innerHTML = `
         <img src="${movie.Poster}" />
-        <h1>${movie.Title} (${movie.Year})</h1>
+        ${movie.Title} (${movie.Year})
         `;
-        document.querySelector('#target').appendChild(movieDiv);
+        resultsWrapper.appendChild(option);
     }
 };
 
