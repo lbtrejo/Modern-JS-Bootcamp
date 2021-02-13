4fc0c99dd7aa39c89da7f130c77daeb513c01b3a
Levi Trejo <levi.b.trejo@gmail.com>
2021-02-13 14:05:49 -0600

    feat: grid gen, vert/horiz, abstract dimens, setup step function

--- maze-app/index.js
+++ maze-app/index.js
@@ -6,6 +6,7 @@ const {
     Bodies,
     } = Matter;
 
+const cells = 3;
 const width = 600;
 const height = 600;
 
@@ -33,3 +34,40 @@ const walls = [
 ]
 
 World.add(world, walls);
+
+// Maze generation
+
+const grid = Array(cells)
+    .fill(null)
+    .map(() => Array(cells).fill(false));
+
+const verticals = Array(cells)
+    .fill(null)
+    .map(() => Array(cells - 1).fill(false));
+
+const horizontals = Array(cells - 1)
+    .fill(null)
+    .map(() => Array(cells).fill(false));
+
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+const stepThroughCell = (row, column) => {
+    // If I have visited the cell at [row, column], then return
+
+    // Mark this cell as being visited (tracked via grid array)
+
+    // Assemble randomly-ordered list of neighbors
+
+    // For each neighbor...
+
+    // See if neighbor is out of bounds
+
+    // If we have visited that neighbor, continue to next neighbor
+
+    // Remove a wall from either horizontals or verticals
+
+    // Visit that next cell
+}
+
+stepThroughCell(startRow, startColumn);
