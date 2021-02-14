3053b234faf819e6c58f0b44da12782088ba1877
Levi Trejo <levi.b.trejo@gmail.com>
2021-02-14 12:27:32 -0600

    feat: iterating over alls, drawing vertical/horizontal segments

--- maze-app/index.js
+++ maze-app/index.js
@@ -10,6 +10,9 @@ const cells = 3;
 const width = 600;
 const height = 600;
 
+const unitLength = width / cells;
+
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
@@ -106,14 +109,45 @@ const stepThroughCell = (row, column) => {
         } else {
             horizontals[row][column] = true;
         }
-        
+        // Visit that next cell
         stepThroughCell(nextRow, nextColumn);
     }
-
-    // Visit that next cell
 }
 
-
 stepThroughCell(startRow, startColumn);
-console.log(verticals);
-console.log(horizontals);
+
+horizontals.forEach((row, rowIndex) => {
+    row.forEach((open, columnIndex) => {
+        if (open) {
+            return;
+        }
+
+        const wall = Bodies.rectangle(
+            columnIndex * unitLength + unitLength / 2,
+            rowIndex * unitLength + unitLength,
+            unitLength,
+            10,
+            { isStatic: true}
+        );
+
+        World.add(world, wall);
+    })
+})
+
+verticals.forEach((row, rowIndex) => {
+    row.forEach((open, columnIndex) => {
+        if (open) {
+            return;
+        }
+
+        const wall = Bodies.rectangle(
+            columnIndex * unitLength + unitLength,
+            rowIndex * unitLength + unitLength / 2,
+            10,
+            unitLength,
+            { isStatic: true }
+        );
+    
+        World.add(world, wall);
+    })
+})
