9281ebfaaeb4e563aa85a966fe8e822644f26908
Levi Trejo <levi.b.trejo@gmail.com>
2021-02-14 12:50:23 -0600

    feat: keypresses and keyboard controls

--- maze-app/index.js
+++ maze-app/index.js
@@ -1,12 +1,6 @@
-const { 
-    Engine, 
-    Render, 
-    Runner, 
-    World, 
-    Bodies,
-    } = Matter;
+const { Engine, Render, Runner, World, Bodies,Body } = Matter;
 
-const cells = 15;
+const cells = 5;
 const width = 600;
 const height = 600;
 
@@ -170,3 +164,32 @@ const ball = Bodies.circle(
     unitLength / 4,
 )
 World.add(world, ball);
+
+document.addEventListener('keydown', event => {
+    const { x, y } = ball.velocity;
+
+    if (event.keyCode === 87) {
+        Body.setVelocity(ball, {
+            x,
+            y: y - 5
+        })
+    }
+    else if (event.keyCode === 83) {
+        Body.setVelocity(ball, {
+            x,
+            y: y + 5
+        })
+    }
+    else if (event.keyCode === 65) {
+        Body.setVelocity(ball, {
+            x: x - 5,
+            y
+        })
+    }
+    else if (event.keyCode === 68) {
+        Body.setVelocity(ball, {
+            x: x + 5,
+            y
+        })
+    }
+})
