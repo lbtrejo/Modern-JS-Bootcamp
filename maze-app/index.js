d8fb279eda1cae5249759265ab467eaca29d399a
Levi Trejo <levi.b.trejo@gmail.com>
2021-02-14 11:40:31 -0600

    feat: movement direction, updating walls, validating wall structure

--- maze-app/index.js
+++ maze-app/index.js
@@ -78,22 +78,42 @@ const stepThroughCell = (row, column) => {
 
     // Assemble randomly-ordered list of neighbors
     const neighbors = shuffle([
-        [row - 1, column],
-        [row, column + 1],
-        [row + 1, column],
-        [row, column - 1]
+        [row - 1, column, 'up'],
+        [row, column + 1, 'right'],
+        [row + 1, column, 'down'],
+        [row, column - 1, 'left']
     ]);
     
     // For each neighbor...
+    for (let neighbor of neighbors) {
+        const [nextRow, nextColumn, direction] = neighbor;
 
-    // See if neighbor is out of bounds
-
-    // If we have visited that neighbor, continue to next neighbor
-
-    // Remove a wall from either horizontals or verticals
+        // See if neighbor is out of bounds
+        if(nextRow < 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells) {
+            continue;
+        }
+        // If we have visited that neighbor, continue to next neighbor
+        if (grid[nextRow][nextColumn]) {
+            continue;
+        }
+        // Remove a wall from either horizontals or verticals
+        if (direction === 'left') {
+            verticals[row][column - 1] = true;
+        } else if (direction === 'right') {
+            verticals[row][column] = true;
+        } else if (direction === 'up') {
+            horizontals[row - 1][column] = true;
+        } else {
+            horizontals[row][column] = true;
+        }
+        
+        stepThroughCell(nextRow, nextColumn);
+    }
 
     // Visit that next cell
 }
 
-// stepThroughCell(startRow, startColumn);
-stepThroughCell(1, 1);
+
+stepThroughCell(startRow, startColumn);
+console.log(verticals);
+console.log(horizontals);
