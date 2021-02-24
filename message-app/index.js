5e03d05eb27bd0f261b1ef13fee0be569b9e5217
Levi Trejo <levi.b.trejo@gmail.com>
2021-02-24 12:03:15 -0600

    feat: last rule update

--- message-app/index.js
+++ message-app/index.js
@@ -6,15 +6,15 @@ if (hash) {
     document.querySelector('h1').innerText = atob(hash.slice(1));
 }
 
-document.querySelector('form').addEventListener('submit', event => {
+document.querySelector('form').addEventListener('submit', (event) => {
     event.preventDefault();
 
-    document.querySelector('#message-form').classList.add('hide')
-    document.querySelector('#link-form').classList.remove('hide')
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#link-form').classList.remove('hide');
 
     const input = document.querySelector('#message-input');
     const encodedData = btoa(input.value);
     const linkInput = document.querySelector('#link-input');
     linkInput.value = `${window.location}#${encodedData}`;
     linkInput.select();
-})
+});
