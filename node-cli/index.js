031b590575b86db6208d1e9196ff14963286d526
Levi Trejo <levi.b.trejo@gmail.com>
2021-02-25 14:41:37 -0600

    feat: sub-optimal solution 1

--- node-cli/index.js
+++ node-cli/index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const fs = require('fs');
 
 fs.readdir(process.cwd(), (err, filenames) => { // '.' indicates the CWD, only usable
@@ -15,5 +17,35 @@ fs.readdir(process.cwd(), (err, filenames) => { // '.' indicates the CWD, only u
         console.error(err);
     }
 
-    console.log(filenames);
+    // Bad code here!!!
+    const allStats = Array(filenames.length).fill(null);
+
+    for (let filename of filenames) {
+        const index = filenames.indexOf(filename);
+
+        fs.lstat(filename, (err, stats) => {
+            if (err) {
+                console.log(err);
+            }
+            allStats[index] = stats;
+            const ready = allStats.every((stats) => {
+                return stats;
+            });
+
+            if (ready) {
+                allStats.forEach((stats, index) => {
+                    console.log(filenames[index], stats.isFile());
+                });
+            }
+        });
+    }
+
+    // This code is bad because it can return the results logging in a random order
+    // Since the HD takes different lengths of time to return the callback,
+
+    // Bad code complete
+
+    // filenames.map((file) => {
+    //     fs.lstat(file);
+    // });
 });
