33c4051796066b39a8041612ca3a9862b1892129
Levi Trejo <levi.b.trejo@gmail.com>
2021-02-25 15:25:30 -0600

    feat: suboptimal solution #2, 3 methods

--- node-cli/index.js
+++ node-cli/index.js
@@ -1,8 +1,15 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const util = require('util');
 
-fs.readdir(process.cwd(), (err, filenames) => { // '.' indicates the CWD, only usable
+// Method #2
+// const lstat = util.promisify(fs.lstat);
+
+// Method #3
+const { lstat } = fs.promises;
+
+fs.readdir(process.cwd(), async (err, filenames) => { // '.' indicates the CWD, only usable
     // on *nix based machines, used process.cwd() instead
 
     // Either
@@ -17,31 +24,42 @@ fs.readdir(process.cwd(), (err, filenames) => { // '.' indicates the CWD, only u
         console.error(err);
     }
 
-    // Bad code here!!!
-    const allStats = Array(filenames.length).fill(null);
-
     for (let filename of filenames) {
-        const index = filenames.indexOf(filename);
-
-        fs.lstat(filename, (err, stats) => {
-            if (err) {
-                console.log(err);
-            }
-            allStats[index] = stats;
-            const ready = allStats.every((stats) => {
-                return stats;
-            });
-
-            if (ready) {
-                allStats.forEach((stats, index) => {
-                    console.log(filenames[index], stats.isFile());
-                });
-            }
-        });
+        try { 
+            const stats = await lstat(filename);
+            console.log(filename, stats.isFile());
+        }
+        catch (err) {
+            console.log(err);
+        }
     }
 
-    // This code is bad because it can return the results logging in a random order
-    // Since the HD takes different lengths of time to return the callback,
+    // Bad code here!!!
+
+    // Suboptimal solution 1:
+    // Any added complexity into the for loop would get messy in a hurry
+
+    // const allStats = Array(filenames.length).fill(null);
+
+    // for (let filename of filenames) {
+    //     const index = filenames.indexOf(filename);
+
+    //     fs.lstat(filename, (err, stats) => {
+    //         if (err) {
+    //             console.log(err);
+    //         }
+    //         allStats[index] = stats;
+    //         const ready = allStats.every((stats) => {
+    //             return stats;
+    //         });
+
+    //         if (ready) {
+    //             allStats.forEach((stats, index) => {
+    //                 console.log(filenames[index], stats.isFile());
+    //             });
+    //         }
+    //     });
+    // }
 
     // Bad code complete
 
@@ -49,3 +67,17 @@ fs.readdir(process.cwd(), (err, filenames) => { // '.' indicates the CWD, only u
     //     fs.lstat(file);
     // });
 });
+
+// Suboptimal solution 2:
+// Method #1
+// const lstat = (filename) => {
+//     return new Promise((resolve, reject) => {
+//         fs.lstat(filename, (err, stats) => {
+//             if (err) {
+//                 reject(err);
+//             }
+
+//             resolve(stats);
+//         })
+//     });
+// };
