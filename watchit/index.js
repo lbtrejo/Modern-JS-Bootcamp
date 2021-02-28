768d4b9cb9c9509f6c57560625f2da05b1346c4f
Levi Trejo <levi.b.trejo@gmail.com>
2021-02-28 15:31:50 -0600

    feat: added spawn child proc, app functional

--- watchit/index.js
+++ watchit/index.js
@@ -3,21 +3,29 @@
 const chokidar = require('chokidar');
 const debounce = require('lodash.debounce');
 const program = require('caporal');
-
-const start = debounce(() => {
-    console.log('Starting users program');
-}, 100);
+const fs = require('fs');
+const { spawn } = require('child_process');
 
 program
     .version('0.0.1')
     .argument('[filename]', 'Name of a file to execute')
-    .action((args) => {
-        console.log(args);
+    .action(async ({ filename }) => {
+        const name = filename || 'index.js';
+
+        try {
+            await fs.promises.access(name);
+        } catch (err) {
+            throw new Error(`Could not find the file: ${name}`);
+        }
+
+        const start = debounce(() => {
+            spawn('node', [name], { stdio: 'inherit' });
+        }, 100);
+
+        chokidar.watch('.')
+            .on('add', start)
+            .on('change', () => start)
+            .on('unlink', () => start);
     });
 
 program.parse(process.argv);
-
-// chokidar.watch('.')
-//     .on('add', start)
-//     .on('change', () => console.log('file changed'))
-//     .on('unlink', () => console.log('file unlinked'));
