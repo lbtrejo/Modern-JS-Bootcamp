ccda7389786be57dec7bab99112086599ced0e43
Levi Trejo <levi.b.trejo@gmail.com>
2021-02-28 15:11:43 -0600

    feat: added caporal for arg handling and help documentation

--- watchit/index.js
+++ watchit/index.js
@@ -1,8 +1,23 @@
 #!/usr/bin/env node
 
 const chokidar = require('chokidar');
+const debounce = require('lodash.debounce');
+const program = require('caporal');
 
-chokidar.watch('.')
-    .on('add', () => console.log('file added'))
-    .on('change', () => console.log('file changed'))
-    .on('unlink', () => console.log('file unlinked'));
+const start = debounce(() => {
+    console.log('Starting users program');
+}, 100);
+
+program
+    .version('0.0.1')
+    .argument('[filename]', 'Name of a file to execute')
+    .action((args) => {
+        console.log(args);
+    });
+
+program.parse(process.argv);
+
+// chokidar.watch('.')
+//     .on('add', start)
+//     .on('change', () => console.log('file changed'))
+//     .on('unlink', () => console.log('file unlinked'));
