0e13173bce21e23a224d1028e805e510412f86c0
Levi Trejo <levi.b.trejo@gmail.com>
2021-03-01 19:27:52 -0600

    feat: handling post with form data and parsing it manually

--- ecomm/index.js
+++ ecomm/index.js
@@ -1,1 +1,37 @@
-console.log('Hi there!');
+const express = require('express');
+
+const app = express();
+
+// setup a route handler for a get on the root
+app.get('/', (request, response) => {
+    // normally noted as req and res
+    response.send(`
+        <div>
+            <form method="POST" >
+                <input name="email" placeholder="email" />
+                <input name="password" placeholder="password" />
+                <input name="passwordConfirmation" placeholder="password confirmation" />
+                <button>Sign Up</button>
+            </form>
+        </div>
+    `);
+});
+
+// setup a route handler for posting the sign up form data to
+app.post('/', (req, res) => {
+    req.on('data', (data) => {
+        const parsed = data.toString('utf8').split('&');
+        const formData = {}; // initialize a formData object
+        for (const pair of parsed) {
+            // iterate over the parsed data array and create key:value pairs to add to formData
+            const [key, value] = pair.split('=');
+            formData[key] = value;
+        }
+        console.log(formData);
+    });
+    res.send('Account Created!');
+});
+
+app.listen(3000, () => {
+    console.log('Listening');
+});
