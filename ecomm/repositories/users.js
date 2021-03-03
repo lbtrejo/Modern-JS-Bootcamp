8ba730c6b004ceb88bb41965f7d7439c0dc07cbf
Levi Trejo <levi.b.trejo@gmail.com>
2021-03-03 12:37:48 -0600

    feat: create and randomId methods added

--- ecomm/repositories/users.js
+++ ecomm/repositories/users.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const crypto = require('crypto');
 
 class UsersRepository {
     constructor(filename) {
@@ -17,10 +18,32 @@ class UsersRepository {
     async getAll() {
         return JSON.parse(await fs.promises.readFile(this.filename, { encoding: 'utf8' }));
     }
+
+    async create(attrs) {
+        attrs.id = this.randomId();
+
+        // { email: email@domain, pass: pwd }
+        const records = await this.getAll();
+
+        records.push(attrs);
+        // write the update records array to this.filename
+        await this.writeAll(records);
+        // encoding defaults to utf8 and can be ommitted as an option if that encoding is fine
+    }
+
+    async writeAll(records) {
+        await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2));
+    }
+
+    randomId() {
+        return crypto.randomBytes(4).toString('hex');
+    }
 }
 
 const test = async () => {
     const repo = new UsersRepository('users.json');
+
+    await repo.create({ email: 'test@testing.com', password: 'password' });
 
     const users = await repo.getAll();
 
