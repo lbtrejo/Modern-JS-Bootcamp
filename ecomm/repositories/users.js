3c8be209dab8c145c59561ca50270b84e65bf16a
Levi Trejo <levi.b.trejo@gmail.com>
2021-03-03 14:34:12 -0600

    feat: user repo complete with export

--- ecomm/repositories/users.js
+++ ecomm/repositories/users.js
@@ -41,21 +41,59 @@ class UsersRepository {
 
     async getOne(id) {
         const records = await this.getAll();
-        return records.find(record => record.id === id);
+        return records.find((record) => record.id === id);
+    }
+
+    async delete(id) {
+        const records = await this.getAll();
+        const filteredRecords = records.filter((record) => record.id !== id);
+        await this.writeAll(filteredRecords);
+    }
+
+    async update(id, attrs) {
+        const records = await this.getAll();
+        const record = records.find((record) => record.id === id);
+
+        if (!record) {
+            throw new Error(`Record with id ${id} not found...`);
+        }
+
+        Object.assign(record, attrs);
+
+        await this.writeAll(records);
+    }
+
+    async getOneBy(filters) {
+        const records = await this.getAll();
+
+        for (const record of records) {
+            let found = true;
+            // iterate through all K:V pairs of each object
+            for (const key in filters) {
+                if (record[key] !== filters[key]) {
+                    found = false;
+                }
+            }
+            // if a match is found on all pairs, return the record
+            if (found) {
+                return record;
+            }
+        }
     }
 }
 
-const test = async () => {
-    const repo = new UsersRepository('users.json');
+// module.exports = UsersRepository;
+// The above isn't ideal as it would allow for different variations of the repo to be created
+//  ANOTHER FILE
+// const usersRepository = require('./users');
+// const repo = new UsersRepository('users.json');
+// YET ANOTHER FILE
+// const usersRepository = require('./users');
+// const repo = new UsersRepository('user.json');
 
-    await repo.create({ email: 'test@testing.com', password: 'password' });
+// Trying to debug the above scenario would be a nightmare
 
-    const users = await repo.getAll();
+// Instead, we can only export a singular version of the repo
+// for all outside files to interface with
 
-    const single = await repo.getOne('f14d6614');
-
-    console.log(users);
-    console.log(single);
-};
-
-test();
+module.exports = new UsersRepository('users.json');
