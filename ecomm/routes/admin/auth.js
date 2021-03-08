64777f9739d44a191ff26d55b8b6211854e101c9
Levi Trejo <levi.b.trejo@gmail.com>
2021-03-08 14:47:25 -0600

    refactor: html views for MVC design pattern

--- ecomm/routes/admin/auth.js
+++ ecomm/routes/admin/auth.js
@@ -1,22 +1,14 @@
 const express = require('express');
 const usersRepo = require('../../repositories/users');
+const signupTemplate = require('../../views/admin/auth/signup');
+const signinTemplate = require('../../views/admin/auth/signin');
 
 const router = express.Router();
 
 // setup a route handler for a get on the root
 router.get('/signup', (request, response) => {
     // normally noted as req and res
-    response.send(`
-        <div>
-            Your ID is: ${request.session.userId}
-            <form method="POST" >
-                <input name="email" placeholder="email" />
-                <input name="password" placeholder="password" />
-                <input name="passwordConfirmation" placeholder="password confirmation" />
-                <button>Sign Up</button>
-            </form>
-        </div>
-    `);
+    response.send(signupTemplate({ request }));
 });
 
 // setup a route handler for posting the sign up form data to
@@ -37,7 +29,7 @@ router.post('/signup', async (req, res) => {
     // Store the ID of that user inside the users cookie
     req.session.userId = dbUser.id; // Added by cookie session
 
-    res.send(`Account created with id: ${dbUser.id}`);
+    return res.send(`Account created with id: ${dbUser.id}`);
 });
 
 router.get('/signout', (req, res) => {
@@ -46,15 +38,7 @@ router.get('/signout', (req, res) => {
 });
 
 router.get('/signin', (req, res) => {
-    res.send(`
-        <div>
-            <form method="POST" >
-                <input name="email" placeholder="email" />
-                <input name="password" placeholder="password" />
-                <button>Sign In</button>
-            </form>
-        </div>
-    `);
+    res.send(signinTemplate());
 });
 
 router.post('/signin', async (req, res) => {
@@ -75,7 +59,7 @@ router.post('/signin', async (req, res) => {
     req.session.userId = existingUser.id;
 
     // issue response confirming login
-    res.send('You are signed in');
+    return res.send('You are signed in');
 });
 
 module.exports = router;
---
+++ ecomm/views/admin/auth/signin.js
@@ -0,0 +1,8 @@
+module.exports = () => `
+    <div>
+        <form method="POST" >
+            <input name="email" placeholder="email" />
+            <input name="password" placeholder="password" />
+            <button>Sign In</button>
+        </form>
+    </div>`;
