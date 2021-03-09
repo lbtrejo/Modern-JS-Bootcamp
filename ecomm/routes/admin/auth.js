92d71e3baa3ccdca3f5502be21cd4d475af71f56
Levi Trejo <levi.b.trejo@gmail.com>
2021-03-08 18:42:25 -0600

    feat: custom validation functional

--- ecomm/routes/admin/auth.js
+++ ecomm/routes/admin/auth.js
@@ -16,29 +16,40 @@ router.get('/signup', (req, res) => {
 router.post(
     '/signup',
     [
-        check('email').trim().normalizeEmail().isEmail(),
-        check('password').trim().isLength({ min: 4, max: 20 }),
-        check('passwordConfirmation').trim().isLength({ min: 4, max: 20 }),
+        check('email')
+            .trim()
+            .normalizeEmail()
+            .isEmail()
+            .withMessage('Must be a valid email')
+            .custom(async (email) => {
+                const existingUser = await usersRepo.getOneBy({ email });
+                if (existingUser) {
+                    throw new Error('Email in use...');
+                }
+            }),
+        check('password')
+            .trim()
+            .isLength({ min: 4, max: 20 })
+            .withMessage('Must be between 4 and 20 characters'),
+        check('passwordConfirmation')
+            .trim()
+            .isLength({ min: 4, max: 20 })
+            .withMessage('Must be between 4 and 20 characters')
+            .custom((passwordConfirmation, { req }) => {
+                if (passwordConfirmation !== req.body.password) {
+                    throw new Error('Password and confirmation must match');
+                }
+            }),
     ],
     async (req, res) => {
         const errors = validationResult(req);
         console.log(errors);
 
         const { email, password, passwordConfirmation } = req.body;
-        const existingUser = await usersRepo.getOneBy({ email });
-
-        if (existingUser) {
-            return res.send('Email in use');
-        }
-        if (password !== passwordConfirmation) {
-            return res.send('Passwords do not match');
-        }
-
-        // Create a user in the repo for the new user
         const dbUser = await usersRepo.create({ email, password });
 
         // Store the ID of that user inside the users cookie
-        req.session.userId = dbUser.id; // Added by cookie session
+        req.session.userId = dbUser.id;
 
         return res.send(`Account created with id: ${dbUser.id}`);
     },
