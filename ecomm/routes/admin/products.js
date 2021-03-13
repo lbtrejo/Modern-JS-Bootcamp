4f2d75f3ee85eb713cf77e3240b0045060ea33ec
Levi Trejo <levi.b.trejo@gmail.com>
2021-03-13 12:46:16 -0600

    feat: delete functional, CRUD admin operations complete

--- ecomm/routes/admin/products.js
+++ ecomm/routes/admin/products.js
@@ -87,4 +87,10 @@ router.post(
     },
 );
 
+router.post('/admin/products/:id/delete', requireAuth, async (req, res) => {
+    await productsRepo.delete(req.params.id);
+
+    res.redirect('/admin/products');
+});
+
 module.exports = router;
