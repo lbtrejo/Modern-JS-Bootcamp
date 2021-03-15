3f3cb18e2a9756be08337649951f0626000dc362
Levi Trejo <levi.b.trejo@gmail.com>
2021-03-15 10:13:07 -0500

    feat: sample tests, run test methods, basic reporting

--- tme/runner.js
+++ tme/runner.js
@@ -1,9 +1,38 @@
+/* eslint-disable no-restricted-syntax */
 const fs = require('fs');
 const path = require('path');
 
 class Runner {
     constructor() {
         this.testFiles = [];
+    }
+
+    async runTests() {
+        for (const file of this.testFiles) {
+            const beforeEaches = [];
+            global.beforeEach = (fn) => {
+                beforeEaches.push(fn);
+            };
+
+            global.it = (desc, fn) => {
+                beforeEaches.forEach((func) => {
+                    func();
+                });
+                try {
+                    fn();
+                    console.log(`PASSED - ${desc}`);
+                } catch (err) {
+                    console.log(`FAILED - ${desc}`);
+                    console.log('\t', err.message);
+                }
+            };
+
+            try {
+                require(file.name);
+            } catch (err) {
+                console.log(err);
+            }
+        }
     }
 
     async collectFiles(targetPath) {
@@ -18,7 +47,8 @@ class Runner {
             } else if (stats.isDirectory()) {
                 const childFiles = await fs.promises.readdir(filepath);
 
-                files.push(...childFiles.map((f) => path.join(file, f))); // push all elements of childFiles arr into files
+                files.push(...childFiles.map((f) => path.join(file, f)));
+                // push all elements of childFiles arr into files
             }
         }
 
